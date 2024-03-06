9bc05b1d65132a43bb76b676e42c9495b331f314
zaenal <60531825+zaenalriz@users.noreply.github.com>
2024-03-06 08:54:53 +0700

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,13 +1,41 @@
-
-INSERT INTO `users` (`id`, `nama`, `kelas`, `email`, `status_siswa`, `tanggal`) VALUES
-(1, 'Adi Nugroho', 'RPL', 'adi@example.com', 'aktif', '2022-02-04 17:00:00'),
-(2, 'Budi Santoso', 'Multimedia', 'budi@example.com', 'tidak_aktif', '2022-02-11 17:00:00'),
-(3, 'Citra Wijaya', 'TKJ', 'citra@example.com', 'aktif', '2022-02-17 17:00:00'),
-(4, 'Dewi Puspita', 'RPL', 'dewi@example.com', 'aktif', '2022-02-20 17:00:00'),
-(5, 'Eko Prasetyo', 'Multimedia', 'eko@example.com', 'tidak_aktif', '2022-02-09 17:00:00'),
-(6, 'Fitriani Susanto', 'TKJ', 'fitriani@example.com', 'aktif', '2022-02-27 17:00:00'),
-(7, 'Hendra Tan', 'RPL', 'hendra@example.com', 'aktif', '2022-02-14 17:00:00'),
-(8, 'Indah Sari', 'Multimedia', 'indah@example.com', 'tidak_aktif', '2022-02-07 17:00:00'),
-(9, 'Joko Widodo', 'TKJ', 'joko@example.com', 'aktif', '2022-01-31 17:00:00'),
-(10, 'Kartika Dewi', 'RPL', 'kartika@example.com', 'aktif', '2022-02-22 17:00:00');
-
+CREATE table users (
+  id_user INT AUTO_INCREMENT PRIMARY KEY, 
+  nama VARCHAR(255) NOT NULL, 
+  pelajaran VARCHAR(50) NOT NULL, 
+  nilai INT NOT NULL, 
+  tanggal TIMESTAMP DEFAULT NULL
+);
+INSERT INTO users (nama, pelajaran, nilai, tanggal) 
+VALUES 
+  (
+    'Budi Setiawan', 'Matematika', 90, 
+    '2022-03-09'
+  ), 
+  (
+    'Siti Rahayu', 'Matematika', 
+    85, '2022-03-09'
+  ), 
+  (
+    'Ahmad Prabowo', 'Matematika', 78, '2022-03-08'
+  ), 
+  (
+    'Dewi Kusuma', 'Matematika', 95, '2022-03-08'
+  ), 
+  (
+    'Iwan Santoso', 'Fisika', 88, '2022-03-06'
+  ), 
+  (
+    'Anita Susanti','Fisika', 75, '2022-03-02'
+  ), 
+  (
+    'Joko Widodo', 'Fisika', 92, '2022-03-06'
+  ), 
+  (
+    'Ratna Dewi','Kimia', 87, '2022-03-03'
+  ), 
+  (
+    'Surya Pratama', 'Kimia', 80, '2022-03-03'
+  ), 
+  (
+    'Dian Sari', 'Kimia', 96, '2022-03-04'
+  );
